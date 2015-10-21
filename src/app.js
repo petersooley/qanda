8e89a28e1e84b284c15f026ee9fac350e7b04c9e
psoots <peter@castironcoding.com>
2015-10-21 00:07:42 -0700

    [CHORE] Goodbye handlebars. Hello nunjucks.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 var express = require('express');
-var app = express();
 var cons = require('consolidate');
+var nunjucks = require('nunjucks');
 var guestRouter = require('./routes/guest');
 var adminRouter = require('./routes/admin');
 
-app.engine('hbs', cons.handlebars);
+var app = express();
 
-app.set('view engine', 'hbs');
+cons.requires.nunjucks = nunjucks.configure(__dirname+'/views', {
+  autoescape: true,
+});
+
+app.engine('html', cons.nunjucks);
+
+app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 app.use('/', guestRouter);
 app.use('/admin', adminRouter);
+
+app.use(express.static(__dirname + '/assets'));
 
 var server = app.listen(3000, function () {
   console.log("Server running locally on port 3000");
