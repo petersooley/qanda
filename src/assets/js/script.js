63e3026a9c6650b626c9d93342bcd72d09016714
psoots <peter@castironcoding.com>
2015-10-21 23:45:37 -0700

    [FEATURE] Adds client side script. Not saving guest choices yet.

---
+++ src/assets/js/script.js
@@ -0,0 +1,69 @@
+var QUESTS_SELECTOR = 'form[data-quests]';
+var QUESTS_SUBMIT = 'input[type="submit"]';
+var QUESTS_COMPLETE = '[data-quests-complete]';
+
+function each(items, cb) {
+  return Array.prototype.forEach.call(items, cb);
+};
+
+function matches(el, selector) {
+  return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
+};
+
+function hide(el) {
+  el.style.display = 'none';
+}
+
+function show(el) {
+  el.style.display = '';
+}
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+};
+
+function Quest(el) {
+  this.el = el;
+  this.next = null;
+
+  var sibling = el.nextElementSibling;
+  if (sibling && matches(sibling, QUESTS_SELECTOR)) {
+    this.next = sibling;
+  }
+
+  this.el.querySelector(QUESTS_SUBMIT)
+    .addEventListener('click', this.handleSubmit.bind(this));
+
+};
+
+Quest.prototype.handleSubmit = function handleSubmit(event) {
+  event.preventDefault();
+
+  this.submit();
+
+  hide(this.el);
+  if (this.next) {
+    show(this.next);
+  } else {
+    show(document.querySelector(QUESTS_COMPLETE));
+  }
+};
+
+Quest.prototype.submit = function submit() {
+  // todo
+  console.log('submitting...');
+}
+
+
+function start() {
+  var els = document.querySelectorAll(QUESTS_SELECTOR);
+  each(els, function(el, i) {
+    new Quest(el);
+  });
+};
+
+ready(start);
